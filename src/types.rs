43e374b635518c28fb9ecc4da83e088322804bc1
Ndeta Innocent <you@example.com>
2022-02-04 23:57:35 +0200

    types in rust

---
+++ src/types.rs
@@ -0,0 +1,13 @@
+/*
+aPrimitives types-----------
+Integers:u8, u16, i16, i32, i64, u32,u128,(Numbers of bits they take in memory)
+Floats: f32, f64
+Boolean:(bool)
+Characters(char)
+Tuples
+Arrays
+
+*/
+pub fn run(){
+
+}
