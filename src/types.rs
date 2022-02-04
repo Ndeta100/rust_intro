b45efe7d1ba3539dc3942d624d31e969c46ae160
Ndeta Innocent <you@example.com>
2022-02-05 00:13:49 +0200

    using i32 and i64

--- src/types.rs
+++ src/types.rs
@@ -8,6 +8,21 @@ Tuples
 Arrays
 
 */
-pub fn run(){
 
+// Rust is a statically typed language, which means that it must know the types of all variables at compile time, however, the compiler can usually infer what type we want to use based on the value and how we use it
+// use std::i32;
+// use std::i64;
+pub fn run(){
+//default is i32,
+let x=1;
+
+//default is f64
+let y=3.5;
+
+//add explicit type 
+let z: i64=456789876543456789;
+
+//find max size 
+println!("Max i32: {}", std::i32::MAX);
+println!("Max i64: {}", std::i64::MAX);
 }
