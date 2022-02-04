be09d78a440998da42761397fa4c0624be61666c
Ndeta Innocent <you@example.com>
2022-02-04 23:34:15 +0200

    positional arguments in rust

--- src/print.rs
+++ src/print.rs
@@ -2,5 +2,13 @@ pub fn run(){
     //Print to console
     println!("Hello from the print rs file");
 
-    println!("Number: {}", 1)
+    // Basic formatting
+    println!("Number: {}", 1);
+    println!("{} is from  {}", "Ndeta", "Mars");
+
+    // Positional Arguments
+    println!("{0} is from {1} and {0} likes to {2}", "John", "Mars", "code");
+
+    // Named arguments
+    println!("{name} likes to play {activity}", name="Ndeta", activity="Football")
 }
