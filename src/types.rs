f895cb6bae22f65679cd5afbfe35ecccf8f51781
Ndeta Innocent <you@example.com>
2022-02-05 05:25:48 +0200

    chars and bool

--- src/types.rs
+++ src/types.rs
@@ -1,5 +1,5 @@
 /*
-aPrimitives types-----------
+Primitives types-----------
 Integers:u8, u16, i16, i32, i64, u32,u128,(Numbers of bits they take in memory)
 Floats: f32, f64
 Boolean:(bool)
@@ -25,4 +25,17 @@ let z: i64=456789876543456789;
 //find max size 
 println!("Max i32: {}", std::i32::MAX);
 println!("Max i64: {}", std::i64::MAX);
+
+// Boolean
+let is_active: bool = true;
+println!("{:?}",(x,y,z ,is_active));
+
+// get a boolean from an extension 
+let is_greater: bool=10<5;
+
+// characters
+let a1='a'; 
+let face='\u{1F600}';
+println!("{:?}",(x,y,z ,is_greater,a1,face));
+
 }
