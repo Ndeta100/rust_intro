4be80ae89d6ef981416c57ccbf29cb9a4f32fbfb
Ndeta Innocent <you@example.com>
2022-02-07 07:13:31 +0200

    accessing tuples

--- src/tuples.rs
+++ src/tuples.rs
@@ -1,5 +1,6 @@
 //Tuples are grouped together values of different types and//max is 12 elements
 
 pub fn run(){
-
+let person :(&str, &str, i8)=("Ndeta","Mars",37);
+println!("{} id from {} and is {}",person.0,person.1,person.2);
 }
